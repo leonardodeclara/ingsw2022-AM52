8a9a481223d1a748a6dcddc417af7970ee593444
leonardodeclara <leonardo.declara@mail.polimi.it>
2022-03-25 18:57:07 +0100

    fix .idea folder tracking

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -1,8 +1,10 @@
 package it.polimi.ingsw;
 
-import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+//import org.junit.Test;
 
 /**
  * Unit test for simple App.
