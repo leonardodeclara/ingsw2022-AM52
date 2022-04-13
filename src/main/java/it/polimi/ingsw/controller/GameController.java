e7b03c4b2f2362c15c813dbd5cd05b14016f589c
CDCapobianco <cirodavide.capobianco@mail.polimi.it>
2022-04-13 12:45:20 +0200

    added CardController and Server classes

---
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -0,0 +1,42 @@
+package it.polimi.ingsw.controller;
+
+import it.polimi.ingsw.model.ExpertGame;
+import it.polimi.ingsw.model.Game;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GameController {
+    Game GM;
+    String currentPlayer;
+    ArrayList<String> players;
+    public GameController(boolean isExpert) {
+        GM = (isExpert) ? new ExpertGame() : new Game();
+
+    }
+
+    public void setUpPhase(){
+        //attende che il server dia conferma che i giocatori sono connessi e riceve anche l'arrayList string players
+        //la passa anche a Game così può fare addPlayer
+        GM.instantiateGameElements();
+        assignAssistantDeck();
+        assignInitialStudents();
+        currentPlayer = getRandomPlayer();
+    }
+
+    public void assignAssistantDeck(){
+
+    }
+
+    public void assignInitialStudents(){
+
+    }
+
+    private String getRandomPlayer(){
+        Random rand = new Random();
+        String randomPlayer = players.get(rand.nextInt(players.size()));
+        return randomPlayer;
+    }
+
+
+}
