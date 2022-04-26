7e31040bb145d275f5c39e6f4fda36256b081810
CDCapobianco <cirodavide.capobianco@mail.polimi.it>
2022-04-26 14:41:40 +0200

    added Server and ClientHandler

--- src/main/java/it/polimi/ingsw/controller/Server.java
+++ src/main/java/it/polimi/ingsw/controller/Server.java
@@ -1,4 +1,38 @@
 package it.polimi.ingsw.controller;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Server {
+    private int port;
+    public Server(int port) {
+        this.port = port;
+    }
+    public void startServer() {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        ServerSocket serverSocket;
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            System.err.println(e.getMessage()); // Porta non disponibile
+            return;
+        }
+        System.out.println("Server ready");
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                executor.submit(new ClientHandler(socket));
+            } catch(IOException e) {
+                break; // Entrerei qui se serverSocket venisse chiuso
+            }
+        }
+        executor.shutdown();
+    }
+    public static void main(String[] args) {
+        Server server = new Server(1234);
+        server.startServer();
+    }
 }
